a35f7fa47fc4260eeae07d20f45078e7f70da56e
Michal <michal@Michals-Mac.local>
2019-11-18 00:18:24 +0100

    added react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,24 @@
 import React from "react";
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+const PageOne = () => {
+    return <div>PageOne <Link to="/pagetwo">P2</Link></div>
+}
+
+const PageTwo = () => {
+    return <div>PageTwo <Link to="/">P1</Link></div>
+}
+
 const App = () => {
-  return <div>App</div>;
+  return (
+  <div>
+      <BrowserRouter>
+      <Route path="/" exact component={PageOne}/>
+      <Route path="/pagetwo" component={PageTwo}/>
+      </BrowserRouter>
+      
+      </div>)
+      ;
 };
 
 export default App;
