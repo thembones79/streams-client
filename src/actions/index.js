f30155289299af0e15e12d3be5e3588c06860d51
thembones79 <michal.szumnarski@gmail.com>
2019-11-24 14:56:36 +0100

    bulk action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
 
 export const signIn = userId => {
   return {
@@ -22,6 +30,52 @@ export const createStream = formValues => {
       body: formValues
     };
 
-    streams(request);
+    const response = await streams(request);
+    console.log({ response });
+    dispatch({ type: CREATE_STREAM, payload: response });
   };
+};
+
+export const fetchStreams = () => async dispatch => {
+  const request = {
+    endpoint: "/streams",
+    method: "get",
+    body: {}
+  };
+  const response = await streams(request);
+  console.log({ response });
+  dispatch({ type: FETCH_STREAMS, payload: response });
+};
+
+export const fetchStream = id => async dispatch => {
+  const request = {
+    endpoint: `/streams/:${id}`,
+    method: "get",
+    body: {}
+  };
+  const response = await streams(request);
+  console.log({ response });
+  dispatch({ type: FETCH_STREAM, payload: response });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const request = {
+    endpoint: `/streams/${id}`,
+    method: "put",
+    body: formValues
+  };
+  const response = await streams(request);
+  console.log({ response });
+  dispatch({ type: EDIT_STREAM, payload: response });
+};
+
+export const deleteStream = id => async dispatch => {
+  const request = {
+    endpoint: `/streams/${id}`,
+    method: "delete",
+    body: {}
+  };
+  const response = await streams(request);
+  console.log({ response, id });
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
